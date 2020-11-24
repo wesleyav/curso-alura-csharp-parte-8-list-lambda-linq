9e37fa37eb5863609b9b3d3ce0e24e77f6e65aa8
Wesley <wesley.contato@hotmail.com>
2020-11-24 05:04:23 -0300

    Método de extensão em classe genérica

---
+++ ByteBank.SistemaAgencia/Extensoes/ListExtensoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia.Extensoes
+{
+    public static class ListExtensoes<T>
+    {
+        public static void AdicionarVarios(this List<T> lista, params T[] itens)
+        {
+            foreach (T item in itens)
+            {
+                lista.Add(item);
+            }
+        }
+    }
+}
