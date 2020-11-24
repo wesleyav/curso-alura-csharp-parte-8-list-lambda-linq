79c1263847eed19e9b9a5403dfe8305d6fa96fe2
Wesley <wesley.contato@hotmail.com>
2020-11-24 05:19:37 -0300

    Métodos de extensão genéricos

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using ByteBank.SistemaAgencia.Extensoes;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -21,13 +22,7 @@ namespace ByteBank.SistemaAgencia
             idades.Add(38);
             idades.Add(61);
 
-
-            //ListExtensoes.AdicionarVarios(idades, 1, 5687, 1987, 1567, 567);
-
-            idades.AdicionarVarios(5, 448, 7898, 4564);
-            
-
-            //idades.Remove(5);
+            idades.AdicionarVarios(45, 89, 12);
 
             for (int i = 0; i < idades.Count; i++)
             {
